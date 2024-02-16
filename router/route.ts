7cf1e59e10e5852b8093ac35929c30e01dc044bd
Zaki <zakaria.mouchtati01@gmail.com>
2024-02-16 17:41:55 +0100

    adding prisma client && adding the BD

--- router/route.ts
+++ router/route.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 router.post("/chat-bot", (req: Request, res: Response) => {
   let message = req.body;
@@ -8,4 +11,18 @@ router.post("/chat-bot", (req: Request, res: Response) => {
   res.status(200).end();
 });
 
+router.get("/posts",async (req: Request, res: Response) => {
+    const allposts = await prisma.post.findMany()
+    console.log(allposts)
+});
+
+router.post("/posts",async (req: Request, res: Response) => {
+    const post = await prisma.post.create({
+        data:{
+            title:"HELLO WORLD ",
+            content:"LOREM 20"
+        }
+    })
+    console.log(post)
+});
 export default router;
